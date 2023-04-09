8fd19c1bed176bc51d32b192e8b456566e63177f
chebinho <102003644+chebinho@users.noreply.github.com>
2023-04-09 19:55:34 -0300

    morgan tá pronto

--- codigo.js
+++ codigo.js
@@ -208,7 +208,7 @@ function tests(){
     // ? 0 ou 1
     // (A)(?!") = não pode ter " no final
 
-    let Resumido = '(A+B).(A+C)' // = A + B.C
+    let Resumido = '(A+C)".(A.C+D")"' // = A + B.C
 
     const situa_R_0 = /(0(\+|\.)0)|((0\.1)|(1\.0))|((([A-Z]"?)\.0)|(0\.([A-Z]"?)))|((([A-Z])"\.\13)(?!"))|((([A-Z])\.\16"))/g
     // 0+0 0.0 0.1 1.0 A.0 A".0 0.A 0.A" A".A A.A" = 0 !0
@@ -219,17 +219,19 @@ function tests(){
     const situa_R_AZi = /(([A-Z]")\+0)|(0\+([A-Z]"))|(([A-Z]")\.1)|(1\.([A-Z]"))|(([A-Z]")(\+|\.)\10)/g
     // A"+A" A".A" A"+0 0+A" A".1 1.A" = A" !$2$4$6$8$10
 
-    const abisor = /(([A-Z])\+\((\2(\.[A-Z])+)\))|(([A-Z])\.\((\6(\+[A-Z])+)\))/g
-    //A+(A.B) A+(A.A) A+(A.B.A) A.(A+B) A.(A+A) A.(A+B+A) = A !$2$6
+    const abisor = /(([A-Z])\+\((\2(\.[A-Z]"?)+)\))|(([A-Z])\.\((\6(\+[A-Z])+)\))/g
+    //A+(A.B) A+(A.A) A+(A.B.A) A.(A+B) A.(A+A) A.(A+B+A) A.(A+B"+A") = A !$2$6
 
     const distri_adi = /([A-Z])\+\(([A-Z](?<!\1))\.([A-Z](?<!\1|\2))\)/g // A+(B.C) = (A+B).(A+C) !($1+$2).($1+$3)
     const distri_mult = /([A-Z])\.\(([A-Z](?<!\1))\+([A-Z](?<!\1|\2))\)/ // A.(B+C) = A.B+A.C !$1.$2+$1.$3
 
+    const distri_2 = /(\(([A-Z]"?)(\+|\.)([A-Z]"?)\))(\+|\.)(\(([A-Z]"?)\3([A-Z]"?)\))/g
+    // (A+B).(D"+C) = (A.D")+(A.C)+(B.D")+(B.C) !($2$5$7)$3($2$5$8)$3($4$5$7)$3($4$5$8)
+
     const outra_adi = /([A-Z])\+\"\1\.([A-Z](?<!\1))/g // A + Ā.B = A + B
     const outra_mult = /\(([A-Z])\+([A-Z](?<!\1))\)\.\(\1\+([A-Z](?<!\1|\2))\)/g // (A+B).(A+C) = A+B.C
 
-    const morgan_adi = 0 // (A.B)’ = A' + B'
-    const morgan_mult = 0 // (A+B)’ = A' . B'
+    const morgan = /\((([A-Z]"?)(\+|\.)?)+\)"/g // (A.B)’ = A' + B' !(/?/)"
     //
     const comu_adi = 0 // A + B = B + A
     const comu_mult = 0 // A . B = B . A
@@ -258,8 +260,21 @@ function tests(){
             Resumido = Resumido.replace(distri_adi,"($1+$2).($1+$3)")//
         }else if(Resumido.match(distri_mult) != null){  
             Resumido = Resumido.replace(distri_mult,"$1.$2+$1.$3")//
+        }
+
+        else if(Resumido.match(morgan) != null){
+            let a = Resumido.match(morgan)
+            Resumido = Resumido.replace(morgan,'(/?/)"')
+
+            for(l=0;l<a.length;l++){
+                a[l] = a[l].replace(/([A-Z]"?)/g,'$1"')
+                a[l] = a[l].replace(/\((.+)\)\"/g,'$1')
+                a[l] = a[l].replace(/""/g,'')
+                Resumido = Resumido.replace(/\(\/\?\/\)"/,a[l])
+            }
+
         }else{
-            c = 0
+            c = c-1
         }
         conatador += 1
         console.log(Resumido)
