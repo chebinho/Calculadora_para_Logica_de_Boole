2c5a8adf502f0c9762882b5f243a63afa4846165
chebinho <102003644+chebinho@users.noreply.github.com>
2023-12-02 12:22:43 -0300

    deu boa

--- test/script.js
+++ test/script.js
@@ -20,17 +20,53 @@ function calculo(){
     let A = Variaveis[0]
     let B = Variaveis[1]
     let C = Variaveis[2]
-
+    let A_ = (A==false)
+    let B_ = (B==false)
+    let C_ = (C==false)
     //console.log(A+" - "+B+" - "+C)
 
-    let S1 = document.getElementById("S1")
-    let S2 = document.getElementById("S2")
-    let S3 = document.getElementById("S3")
-    let S4 = document.getElementById("S4")
+    //S1 = (A"+B).(A+B")
+    if( (A_||B)&&(A||B_) ){
+        troca_cor("S1","V")
+    }else{
+        troca_cor("S1","F")
+    }
 
+    //S2 = (A"+B+C").(A+B"+C)
+    if( (A_||B||C_)&&(A||B_||C) ){
+        troca_cor("S2","V")
+    }else{
+        troca_cor("S2","F")
+    }
 
+    //S3 = (A"+B").(A+B)
+    if( (A_||B_)&&(A||B) ){
+        troca_cor("S3","V")
+    }else{
+        troca_cor("S3","F")
+    }
+    
+    //S4 = (A"+B"+C").(A+B+C)
+    if( (A_||B_||C_)&&(A||B||C) ){
+        troca_cor("S4","F")
+    }else{
+        troca_cor("S4","V")
+    }
+    
 }
 calculo()
+
+
+function troca_cor(valor,sinal="F"){
+    let Solu = document.getElementById(valor)
+
+    if(sinal == "V"){
+        Solu.style.backgroundColor = "green"
+    }else{
+        Solu.style.backgroundColor = "red"
+    }
+
+}
 
 //S1 = (A"+B).(A+B")
 //S2 = (A"+B+C").(A+B"+C)
