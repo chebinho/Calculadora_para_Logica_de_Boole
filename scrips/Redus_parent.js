da082a1334f3af719c6a78df88ecfc1ad172564a
chebinho <102003644+chebinho@users.noreply.github.com>
2023-12-26 22:02:44 -0300

    descobre outra maneira

--- scrips/Redus_parent.js
+++ scrips/Redus_parent.js
@@ -1,10 +1,10 @@
 function Redus_parent(Valor){
 
     const verifica = /\(([A-Z]"?|\+|\.)*\(([A-Z]"?|\+|\.)*\)([A-Z]"?|\+|\.)*\)/g
-// (()) | (A+C.(A).T)
+    // (()) | (A+C.(A).T)
 
-    const paren_paren = /((\(([A-Z]"?(\+|\.))+[A-Z]"?\)(\+|\.))+\(([A-Z]"?(\+|\.))+[A-Z]"?\))/g
-    // ((B+C").(A+D)) | (A+(B+C").(A+D)) | (A+(B+C").(A+D)+(C+D)) ! $3
+    const paren_paren = /((\(([A-Z]"?(\+|\.))+[A-Z]"?\)(\+|\.))\(([A-Z]"?(\+|\.))+[A-Z]"?\))(((\.|\+)\(([A-Z]"?(\+|\.))+[A-Z]"?\))*)?/g
+    // ((B+C").(A+D)) | (A+(B+C").(A+D)) | (A+(B+C").(A+D)+(C+D)) ! $1 $8
     const A_paren = /((?<!\.)(([A-Z]"?)(\+|\.))+)(\((([A-Z]"?(\+|\.))+([A-Z]"?))\))/g
     // A.(B+C") = A.B+A.C" ! $1 $6
     const paren_A = /(\((([A-Z]"?(\+|\.))+([A-Z]"?))\))(((\+|\.)([A-Z]"?))+(?!\.))/g
@@ -18,34 +18,26 @@ function Redus_parent(Valor){
             Valor = Valor.replace(paren_paren,'(/?/)')
 
             for(l=0;l<a.length;l++){
-                let letra = a[l].match(/([A-Z]"?)/g)
-                let sinais = a[l].match(paren_paren).toString()
-                sinais = sinais.replace(/([A-Z]"?)/g,"")
-                
-                let sinal_meio = sinais.match(/\)(\+|\.)\(/g)
-                for(t=0;t<sinal_meio.length;t++){
-                    sinal_meio[t] = sinal_meio[t].replace(/\(|\)/g,"")
-                }
+                let grupo_calculo = a[l].replace(paren_paren,"$1")
+                let grupo_resto = a[l].replace(paren_paren,"$8")
 
-                sinais = sinais.match(/\(((\+|\.)+)\)/g)
-                for(t=0;t<sinais.length;t++){
-                    sinais[t] = sinais[t].replace(/\(|\)/g,"")
-                }
+                const regex = /(\(([A-Z]"?|\+|\.)+\))(\+|\.)(\(([A-Z]"?|\+|\.)+\))/g
 
-                let grupo_l1 = sinais[0].match(/(\+|\.)/g)
-                let grupo_l2 = sinais[1].match(/(\+|\.)/g)
-                let grupo_resto = [] // shift()
-                if(sinais[3] != undefined){
-                    sinais.shift()
-                    grupo_resto = sinais.shift()   
-                }
+                let grupo_l1 = grupo_calculo.replace(regex,"$1")
+                let sinal_meio = grupo_calculo.replace(regex,"$3")
+                let grupo_l2 = grupo_calculo.replace(regex,"$4")
 
-                console.log(letra)
-                console.log(sinal_meio)
-                console.log(sinais)
-                console.log("//////////")
+                let sinal_g1 = grupo_l1.match(/(\+|\.)/g)
+                let sinal_g2 = grupo_l2.match(/(\+|\.)/g)
+
+                grupo_l1 = grupo_l1.match(/[A-Z]"?/g)
+                grupo_l2 = grupo_l2.match(/[A-Z]"?/g)
+
                 console.log(grupo_l1)
+                console.log(sinal_g1)
+                console.log(sinal_meio)
                 console.log(grupo_l2)
+                console.log(sinal_g2)
                 console.log(grupo_resto)
 
                 Valor = Valor.replace(/\(\/\?\/\)/,a[l])
@@ -66,3 +58,34 @@ function Redus_parent(Valor){
     console.log(" - - - - - - - - - ")
     return Valor
 }
+
+/*
+                let letra = a[l].match(/([A-Z]"?)/g)
+                let sinais = a[l].match(paren_paren).toString()
+                sinais = sinais.replace(/([A-Z]"?)/g,"")
+
+                
+
+                let sinal_meio = sinais.match(/\)(\+|\.)\(/g)
+                for(t=0;t<sinal_meio.length;t++){
+                    sinal_meio[t] = sinal_meio[t].replace(/\(|\)/g,"")
+                }
+
+                sinais = sinais.match(/\(((\+|\.)+)\)/g)
+                for(t=0;t<sinais.length;t++){
+                    sinais[t] = sinais[t].replace(/\(|\)/g,"")
+                }
+
+                let grupo_l1 = sinais[0].match(/(\+|\.)/g)
+                let grupo_l2 = sinais[1].match(/(\+|\.)/g)
+
+                
+                console.log(letra)
+                console.log(sinal_meio)
+                console.log(sinais)
+                console.log("//////////")
+                console.log(grupo_l1)
+                console.log(grupo_l2)
+
+
+*/
